f96aa51402331af2416ac68bac313b7973ae22fa
lgabrielc <kidmeg100@hotmail.com>
2023-03-07 10:51:18 -0500

    Add module contact + migration + model

--- app/Http/Livewire/Contacto.php
+++ app/Http/Livewire/Contacto.php
@@ -6,10 +6,17 @@ use Livewire\Component;
 
 class Contacto extends Component
 {
-    public $nombre,$correo,$mensaje;
+    public $name, $email, $phone, $message;
 
-    public function boton_contactanos(){
-        
+    public function button_contact()
+    {
+        $this->validate([
+            'name' => 'required|min:4',
+            'email' => 'required|email',
+            'phone' => 'numeric|min:9|max:12',
+            'message' => 'required|min:2|max:40'
+        ]);
+        dd($this->name);
     }
 
     public function render()
--- resources/views/livewire/contacto.blade.php
+++ resources/views/livewire/contacto.blade.php
@@ -3,43 +3,54 @@
         <form class=" bg-sky-800 shadow-md rounded-2xl px-8 pt-6 pb-8 mb-4 mx-52">
             <label class="text-center block text-white font-bold text-2xl mb-6" for="nombre">CONTACTANOS </label>
             <div class="mb-4 bg-slate-100 p-4 rounded-lg mx-24">
-                <label class="block text-gray-700 font-bold mb-2" wire:model="nombre">
+                <label class="block text-gray-700 font-bold mb-2">
                     Nombre completo
                 </label>
-                <input
+                <input wire:model="name"
                     class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="nombre" type="text" placeholder="Ingrese su nombre completo">
+                    id="name" type="text" placeholder="Ingrese su nombre completo">
+                @error('name')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="mb-4 bg-slate-100 p-4 rounded-lg mx-24">
                 <div>
-                    <label class="block text-gray-700 font-bold mb-2" wire:model="correo">
+                    <label class="block text-gray-700 font-bold mb-2">
                         Correo electrónico
                     </label>
-                    <input
+                    <input wire:model="email"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="correo" type="email" placeholder="Ingrese su correo electrónico">
-
+                        id="email" type="email" placeholder="Ingrese su correo electrónico">
+                    @error('email')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div>
-                    <label class="block text-gray-700 font-bold mb-2" wire:model="correo">
+                    <label class="block text-gray-700 font-bold mb-2">
                         Telefono
                     </label>
-                    <input
+                    <input wire:model="phone"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="correo" type="email" placeholder="+51 984786532">
-
+                        id="phone" type="phone" placeholder="+51 984786532">
+                    @error('phone')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
             <div class="mb-4 bg-slate-100 p-4 rounded-lg mx-24">
-                <label class="block text-gray-700 font-bold mb-2" wire:model="mensaje">
+                <label class="block text-gray-700 font-bold mb-2">
                     Mensaje
                 </label>
-                <textarea
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="mensaje" placeholder="Ingrese su mensaje"></textarea>
+                <textarea wire:model="message"
+                    class="shadow appearance-none border rounded w-full py-2 px-3
+                    text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="message" placeholder="Ingrese su mensaje"></textarea>
+                @error('message')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="flex items-center justify-between mx-24">
-                <button
+                <button wire:click='button_contact'
                     class="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                     type="button">
                     Enviar
