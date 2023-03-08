f0f05fe5bd6896708ca744243cbd9a80afe9315b
lgabrielc <kidmeg100@hotmail.com>
2023-03-07 23:14:46 -0500

    Delete request, add form contact finish

--- app/Http/Livewire/Contacto.php
+++ app/Http/Livewire/Contacto.php
@@ -10,13 +10,43 @@ class Contacto extends Component
 {
     public $name, $email, $phone, $message;
 
-    public function submitContactForm(ContactRequest $request)
+    public function validateData()
     {
-        $validatedData = $request->validate();
-        // $this->dispatchBrowserEvent('banner-message', [
-        //     'style' => 'success',
-        //     'message' => 'Su solicitud fue enviada con éxito!'
-        // ]);
+        $this->validate(
+            [
+                'name' => 'required|min:4',
+                'email' => 'required|email',
+                'phone' => 'required|min:9|max:12',
+                'message' => 'required|min:2|max:40'
+            ],
+            [
+                'name.required' => 'El nombre es obligatorio',
+                'name.min' => 'El nombre tiene que tener 4 caracteres como mínimo',
+                'email.required' => 'El email es obligatorio',
+                'email.email' => 'El email debe de tener un formato de correo',
+                'phone.required' => 'El telefono es obligatorio',
+                'phone.min' => 'El telefono tiene que tener como mínimo 9 dígitos',
+                'message.required' => 'El mensaje es obligatorio',
+                'message.min' => 'El mensaje tiene que tener como mínimo 2 caracteres',
+                'message.max' => 'El mensaje no puede tener más de 40 caracteres',
+            ]
+        );
+    }
+
+    public function submitContactForm()
+    {
+        $this->validateData();
+        Contact::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'message' => $this->message
+        ]);
+        $this->reset('name', 'email', 'phone', 'message');
+        $this->dispatchBrowserEvent('banner-message', [
+            'style' => 'success',
+            'message' => 'Su solicitud fue enviada con éxito!'
+        ]);
     }
     public function render()
     {
--- resources/views/livewire/contacto.blade.php
+++ resources/views/livewire/contacto.blade.php
@@ -1,6 +1,8 @@
 <div>
     <div class="container relative mx-auto -top-8">
-        <form class="px-8 pt-6 pb-8 mx-0 mb-4 shadow-md bg-sky-800 rounded-2xl xl:mx-52">
+        <form wire:submit.prevent='submitContactForm' enctype="multipart/form-data"
+            class="px-8 pt-6 pb-8 mx-0 mb-4 shadow-md bg-sky-800 rounded-2xl xl:mx-52">
+            @csrf
             <label class="block mt-2 mb-6 text-2xl font-bold text-center text-white" for="nombre">CONTACTANOS </label>
             <div class="p-4 mx-0 mb-4 rounded-lg sm:mx-8 md:mx-12 lg:mx-24 bg-slate-100">
                 <label class="block mb-2 font-bold text-gray-700">
@@ -33,7 +35,7 @@
                     </label>
                     <input wire:model="phone"
                         class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                        id="number" type="number" placeholder="+51 984786532">
+                        id="phone" type="number" placeholder="+51 984786532">
                     @error('phone')
                         <div class="text-red-600">{{ $message }}</div>
                     @enderror
@@ -51,9 +53,9 @@
                 @enderror
             </div>
             <div class="flex items-center justify-between mx-0 mb-4 sm:mx-8 md:mx-12 lg:mx-24">
-                <button wire:click='submitContactForm'
+                <button
                     class="px-4 py-2 font-bold text-white bg-black rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-                    type="button">
+                    type="submit">
                     Enviar
                 </button>
             </div>
